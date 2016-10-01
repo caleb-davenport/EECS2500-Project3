1224217ad5320eaed01b7ab4f905095262f29f5b
caleb-davenport <caleb-davenport@users.noreply.github.com>
2016-09-30 20:21:19 -0400

    Created Proposer and Acceptor classes

---
+++ src/stablematching/Acceptor.java
@@ -0,0 +1,11 @@
+package stablematching;
+
+import java.util.LinkedList;
+
+public class Acceptor extends Person {
+    
+    public Acceptor(String name, LinkedList preferences) {
+        super(name, preferences);
+    }
+    
+}
---
+++ src/stablematching/Proposer.java
@@ -0,0 +1,12 @@
+package stablematching;
+
+import java.util.LinkedList;
+
+public class Proposer extends Person {
+    
+    public Proposer(String name, LinkedList preferences) {
+        super(name, preferences);
+    }
+    
+    
+}
