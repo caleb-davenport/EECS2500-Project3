d4fc31b4bd226ae6d1baccfc88560d076eef7ba2
caleb-davenport <caleb-davenport@users.noreply.github.com>
2016-09-30 21:12:39 -0400

    Changes partner of person class to private Added appropriate get and set methods for partner

--- src/stablematching/Person.java
+++ src/stablematching/Person.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public abstract class Person {
     public final String     name;
     public final LinkedList preferences;
-    protected    Person     partner;
+    private      Person     partner;
     Person(String name, LinkedList preferences) {
         this.name = name;
         this.preferences = preferences;
@@ -13,6 +13,14 @@ public abstract class Person {
     
     public void removePartner() {
         partner = null;
+    }
+    
+    public void setPartner(Person partner) {
+        this.partner = partner;
+    }
+    
+    public Person getPartner() {
+        return this.partner;
     }
      
 }
